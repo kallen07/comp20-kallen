1f5a4b9c87dfe54ef95dff18bb2487e71fe273a1
Kalina Allen <kalina.allen@tufts.edu>
2015-03-09 16:42:15 -0400

    started working on javascript

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,26 @@
-// Your JavaScript goes here...
+// Create instance of XHR object
+xhr = new XMLHttpRequest();
+
+// Set up the request
+xhr.open("get", "https://github.com/tuftsdev/comp20-kallen/blob/gh-pages/messages/data.json", true);
+
+// Set up handler for the response
+xhr.onreadystatechange = myCallbackFunction;
+
+// Execute the request
+xhr.send();
+
+function myCallbackFunction() {
+console.log("In my callback function, readyState = " + xhr.readyState);
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		console.log)"got data back!");
+		data = JSON.parse(xhr.responseText);
+
+		var messages;
+		for (i = 0; i < data.length; i++) {
+			result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
+		}
+		document.getElementById("message").innerHTML = messages;
+		
+		}
+}
