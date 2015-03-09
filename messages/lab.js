95487fcb9c7232a882ed9c415adef59a178fc934
Kalina Allen <kalina.allen@tufts.edu>
2015-03-09 18:14:50 -0400

    finished part 1 of lab

--- messages/lab.js
+++ messages/lab.js
@@ -1,24 +1,26 @@
-// Create instance of XHR object
-xhr = new XMLHttpRequest();
+function parse() {
+	// Create instance of XHR object
+	xhr = new XMLHttpRequest();
 
-// Set up the request
-var url = "http://tuftsdev.github.io/comp20-kallen/messages/data.json";
-xhr.open("get", url , true);
+	// Set up the request
+	var url = "http://tuftsdev.github.io/comp20-kallen/messages/data.json";
+	xhr.open("get", url , true);
 
-// Execute the request
-xhr.send();
+	// Execute the request
+	xhr.send();
 
-// Set up handler for the response
-xhr.onreadystatechange = function() {
-	if (xhr.readyState == 4 && xhr.status == 200) {
-		data = JSON.parse(xhr.responseText);
+	// Set up handler for the response
+	xhr.onreadystatechange = function()  {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			data = JSON.parse(xhr.responseText);
 
-		var messages = "";
-		for (i = 0; i < data.length; i++) {
-			messages += "<p>" + data[i]['username'] + ": " + data[i]["content"] + "</p>";
+			var messages = "";
+			for (i = 0; i < data.length; i++) {
+				messages += "<p>" + data[i]['username'] + ": " + data[i]["content"] + "</p>";
+			}
+
+			document.getElementById("messages").innerHTML = messages;
+			
 		}
-
-		document.getElementById("messages").innerHTML = messages;
-		
 	}
 }
