a515f42641c5049265db49b1ec09e6e2c115b10a
Kalina Allen <kalina.allen@tufts.edu>
2015-03-10 11:48:16 -0400

    debugging login date to personal icon

--- mmap/MaraudersMap.js
+++ mmap/MaraudersMap.js
@@ -112,7 +112,7 @@ function renderMap()
 	google.maps.event.addListener(marker, 'click', function() {
 		infowindow.close();
 		console.log(login);
-		infowindow.setContent(login);
+		infowindow.setContent("<strong"> + title + " " + login +"</strong>");
     	infowindow.open(map, marker);
 	});
 
